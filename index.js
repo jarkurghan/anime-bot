67e04453641f43b31f27843d8b8d3b81cb28cfb7
jarkurghan <jarkurghan@gmail.com>
2025-04-08 12:37:00 +0500

    new commit

--- index.js
+++ index.js
@@ -41,6 +41,43 @@ bot.start(start);
 //     }
 // });
 
+// bot.command("changeanime", async (ctx) => {
+//     try {
+//         const adminChatId = process.env.ADMIN_CHAT_ID;
+//         const senderId = ctx.from.id;
+
+//         if (String(senderId) !== "6389479517") return ctx.reply("❌ Siz bu buyruqni bajarishga ruxsatga ega emassiz.");
+
+//         const commandParts = ctx.message.text.split(" ");
+//         if (commandParts.length < 4) return ctx.reply("❌ Noto'g'ri format. To'g'ri format: /changeanime <post1ID> <post2ID> <name>");
+
+//         const post1ID = parseInt(commandParts[1], 10);
+//         const post2ID = parseInt(commandParts[2], 10);
+//         const name = commandParts.slice(3).join(" ");
+
+//         if (isNaN(post1ID) || isNaN(post2ID)) return ctx.reply("❌ post1ID va post2ID raqam bo'lishi kerak.");
+//         const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+//         for (let postId = post1ID; postId <= post2ID; postId++) {
+//             try {
+//                 const message =
+//                     `<i>${name}\n<b>${postId - 1160}-qism</b>\n🎙 ${postId - 1160 > 195 ? "Lego" : "Anibla"}</i>\n\n@aniuz_bot\n` +
+//                     `<blockquote>Bot yangiliklaridan xabardor bo\'lish uchun @ani_uz_news kanaliga a\'zo bo\'ling!</blockquote>`;
+//                 await bot.telegram.editMessageCaption(process.env.CHANNEL_ID, postId, null, message, { parse_mode: "HTML" });
+//                 console.log(`✅ Post ${postId} muvaffaqiyatli o'zgartirildi.`);
+//             } catch (error) {
+//                 console.error(`❌ Post ${postId} o'zgartirilayotganda xato yuz berdi:`, error.message);
+//             }
+//             if ((postId - post1ID + 1) % 20 === 0) await delay(43000);
+//         }
+
+//         // Javob qaytarish
+//         ctx.reply(`✅ Postlar ${post1ID} dan ${post2ID} gacha "${name}" matniga o'zgartirildi.`);
+//     } catch (error) {
+//         console.error("❌ Xatolik yuz berdi:", error.message);
+//         ctx.reply("❌ Xatolik yuz berdi. Iltimos, keyinroq urinib ko'ring.");
+//     }
+// });
+
 bot.action(/^anime_(\d+)$/, handleMessage, selectAnime);
 bot.action(/^anime_list_(\d+)$/, handleMessage, changePage);
 bot.action(/^back_anime_list$/, handleMessage, backToAnime);
