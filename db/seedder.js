b240f7470350dcd06b23bd49bb40370b3f2def62
jarkurghan <jarkurghan@gmail.com>
2025-04-17 13:11:23 +0500

    new commit

---
+++ db/seedder.js
@@ -0,0 +1,42 @@
+const knex = require("./db");
+const path = require("path");
+const fs = require("fs");
+
+async function exportData() {
+    try {
+        const logsDir = path.join(__dirname, `seeds`);
+        if (!fs.existsSync(logsDir)) fs.mkdirSync(logsDir);
+
+        const tables = ["user", "anime", "anime_info", "dub", "episode", "channel_post", "user_page"];
+        for (let i = 0; i < tables.length; i++) {
+            const table = tables[i];
+            try {
+                await fs.promises.writeFile(
+                    `${logsDir}/${i + 1}_${table}.js`,
+                    `/**
+ * @param {import("knex").Knex} knex
+ * @returns {Promise<void>}
+ */
+exports.seed = async function (knex) {
+    try {
+        const data = ${JSON.stringify(await knex(table).select("*"), null, 4)};
+        await knex("${table}").insert(data);
+    } catch (error) {
+        console.error(error);
+    }
+};`,
+                    "utf8"
+                );
+                console.log(table, "tayyor");
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    } catch (error) {
+        console.error(error);
+    } finally {
+        await knex.destroy();
+    }
+}
+
+exportData();
